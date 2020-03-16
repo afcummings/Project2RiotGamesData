3b9bbe3a771510b91e929de0947654ad58f14eed
afcummings <afcummings7@gmail.com>
2020-03-16 19:35:51 -0400

    commit with all changes and working code

--- server.py
+++ server.py
@@ -1,28 +1,68 @@
-from flask import Flask, jsonify, render_template
 import pandas as pd
 from sodapy import Socrata
+from flask import Flask, jsonify, render_template
 
-### create a flask instance
-app = Flask(__name__)
-# Example authenticated client (needed for non-public datasets):
+
 client = Socrata("data.richmondgov.com",
                  "9YLbBPExr34rv8so3YWrgKlAM")
-                 #userame="afcummings7@gmail.com.com",
-                 #password="Deepshrub860")
+                #username and password obscured for security reasons
+                 #userame="##########",
+                 #password="###########")
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
 results = client.get("rvtv-zgyc", limit=1000)
-#print(results)
 
 
-# Convert to pandas DataFrame
 results_df = pd.DataFrame.from_records(results)
-#results_df
+results_df
 
+for row in results_df.iterrows():
+    print(results_df.columns)
+    #print(results_df.iloc[0]['location_1'])
+    
 results_df = results_df.drop(results_df.columns[[0,1,2,3,4,5,6,7,9]], axis=1)
 results_df = results_df.dropna(subset=["location_1"])
-#results_df
+
+lat_cor = []
+long_cor = []
+
+import json
+results_df["location_1"]
+
+for index, row in results_df.iterrows():
+    
+    str2 = "latitude"
+    str3 = "longitude"
+    str4 = "human"
+    
+    test1 = [row['location_1']]
+    test2 = json.dumps(test1)
+    
+   # test2 = str(test2)
+   # print(type(test2))
+   # print(test2)
+    
+    intStart = test2.find(str2)+12
+    intEnd = test2.find(str3)-4
+    
+    intStart1=intEnd+17
+    intEnd1 = test2.find(str4)-4
+    
+    test3 = test2[intStart1:intStart1+(intEnd1-intStart1)]
+    test2 = test2[intStart:intStart+(intEnd-intStart)]
+    
+    
+    print(test2, test3)
+    lat_cor.append(test2)
+    long_cor.append(test3)
+    #print(test2.find(str2))
+    
+results_df["lat"] = lat_cor
+results_df['long'] = long_cor
+
+#csv is converted to geojson through https://www.convertcsv.com/csv-to-geojson.html
+
+results_df = results_df.drop(results_df.columns[[0]], axis=1)
+results_df.to_csv("C:\\Users\\AFCummings\\git\\Project2RiotGamesData\\Resources\\deli.csv")
 
 results_list = results_df.values.tolist()
 
@@ -33,28 +73,18 @@ column_list.remove("Lat")
 column_list.remove("Long")
 print(column_list)
 crime_df["sum"] = crime_df[column_list].sum(axis=1)
-#crime_df
-
+crime_df
 
 crime_df = crime_df.drop(crime_df.columns[[0,1,2,3,4,5,6,7,8,9]], axis = 1)
 crime_df.dropna()
+
 crime_list = crime_df.values.tolist()
 
 crime_by_type = pd.read_csv("C:\\Users\\AFCummings\\git\\Project2RiotGamesData\\Resources\\Crime Information.csv")
-crime_by_type = crime_by_type.drop(crime_by_type.columns[[0,10,11]], axis=1)
-#crime_by_type
 
-#def sum_columns(column_name):
-    #crime_by_type.loc['Total'] = pd.Series(crime_by_type[f"{column_name}"].sum(), index = [f"{column_name}"])
-#sum_columns('HOMICIDE')
-#sum_columns('SEX_OFFENSE')
-#sum_columns('ROBBERY')
-#sum_columns('ASSAULT')
-#sum_columns('BURGLARY')
-#sum_columns('VICE')
-#sum_columns('THEFT')
-#sum_columns('VEH_THEFT')
-#sum_columns('OTHER')
+crime_by_type = crime_by_type.drop(crime_by_type.columns[[0,10,11]], axis=1)
+crime_by_type
+
 def crime_sum_type(column_name, new_column):
     crime_by_type[f"{new_column}"] = crime_by_type[f"{column_name}"].sum(axis=0)
 crime_sum_type('HOMICIDE', 'homicide_tot')
@@ -65,16 +95,18 @@ crime_sum_type('BURGLARY', 'burglary_tot')
 crime_sum_type('VICE', 'vice_tot')
 crime_sum_type('THEFT', 'theft_tot')
 crime_sum_type('VEH_THEFT', 'veh_theft_tot')
-crime_sum_type('OTHER', 'other_tot')    
+crime_sum_type('OTHER', 'other_tot')  
 
+crime_by_type
 crime_by_type = crime_by_type[['homicide_tot','sex_off_total','rob_total','assault_tot','burglary_tot','vice_tot','theft_tot',
                               'veh_theft_tot','other_tot']]
-
 crime_by_type = crime_by_type[:1]
 crime_type_list = crime_by_type.values.tolist()
 
 crime2017 = pd.read_csv("C:\\Users\\AFCummings\\git\\Project2RiotGamesData\\Resources\\Crime Information2017-2018.csv")
+
 crime2017 = crime2017.drop(crime2017.columns[[0]], axis=1)
+crime2017
 
 def crime_2017_type(column_name, new_column):
     crime2017[f"{new_column}"] = crime2017[f"{column_name}"].sum(axis=0)
@@ -93,6 +125,20 @@ crime2017 = crime2017[['homicide_tot','sex_off_total','rob_total','assault_tot',
 crime2017 = crime2017[:1]
 crime_2017_list = crime2017.values.tolist()
 
+@app.route("/")
+def home():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+@app.route("/pie.html")
+def pie_method():
+    message = "Hello, World"
+    return render_template('pie.html', message=message)
+@app.route("/map.html")
+def map_method():
+    message = "Hello World"
+    return render_template('map.html', message=message)
+
+#ideally these would have been used for JS
 @app.route("/api/crimedata")
 def crime_data_method():
     return jsonify(crime_list)
@@ -109,10 +155,6 @@ def crime_pie_method():
 def crime_2017_method():
     return jsonify(crime_2017_list)
 
-@app.route("/")
-def home():
-    message = "This is a placeholder"
-    #return render_template here
-
+### A required way of saying "Start the server"
 if __name__ == "__main__":
     app.run(debug=True)
